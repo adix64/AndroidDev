995c5aaff2fadb6a43d053998bac4c8809a6645b
Alex Dinu <adix64@gmail.com>
2020-09-14 15:30:40 +0300

    primul commit

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    public Transform cutie;
+    Vector3 initPos;
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = cutie.transform.position;
+        rigidBody = cutie.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DropObject()
+    {
+        rigidBody.useGravity = true;
+    }
+
+    public void ResetPosition()
+    {
+        rigidBody.useGravity = false;
+        rigidBody.position = initPos;
+    }
+
+}
